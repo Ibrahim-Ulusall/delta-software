65c2751c198160342d5727ed17f3fb329b08b999
ibrahim ulusal <ibrahimulusal185@gmail.com>
2022-11-04 22:34:55 +0300

    accounts uygulası url'e eklendi.

--- deltasoftware/urls.py
+++ deltasoftware/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('lessons.urls')),
+    path('accounts/',include('accounts.urls')),
 ]
