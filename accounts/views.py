cf6b810f2414da7f1f2492b287f1d96afe69d6e9
ibrahim ulusal <ibrahimulusal185@gmail.com>
2022-11-05 14:33:03 +0300

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate ,login, logout
-from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
 
-def login(request):
-    
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request,username = username, password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/index.html',{
+                'hata':'Kullanıcı adi veya parola hatalı!'
+            })
+
+
     return render(request,'accounts/index.html')    
 
 def register(request):
