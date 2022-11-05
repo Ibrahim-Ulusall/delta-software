ac5678c8e853a5e59ed9ef9d608cb46a71a1795a
ibrahim ulusal <ibrahimulusal185@gmail.com>
2022-11-05 16:09:37 +0300

    register.html update

--- accounts/views.py
+++ accounts/views.py
@@ -17,9 +17,35 @@ def login_request(request):
             return render(request,'accounts/index.html',{
                 'hata':'Kullanıcı adi veya parola hatalı!'
             })
-
-
     return render(request,'accounts/index.html')    
 
 def register(request):
+
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['passowrd']
+        re_password = request.POST['re-password']
+
+        userInput = [firstname,lastname,username,email,password,re_password]
+
+        for userinput in userInput:
+            if userinput == '':
+                return render(request,'accounts/register.html',{
+                    'ValueError':'Tüm Alanlar Doldurulmalıdır.'
+                })
+        if '@gmail.com' not in email or '@hotmail.com' not in email:
+            return render(request,'accounts/register.html',{
+                'FormatError':'Lütfen İstenilen Formatta mail adresi giriniz.'
+            })    
+        if password != re_password:
+            return render(request,'accounts/register.html',{
+                'EqualsError':'Parolalar Uyuşmuyor.'
+            })
+        else:
+            pass
+
+
     return render(request,'accounts/register.html')
