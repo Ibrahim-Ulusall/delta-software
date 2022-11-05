20d91645d3a06e114d04a9e254c533ccd6d75a90
ibrahim ulusal <ibrahimulusal185@gmail.com>
2022-11-05 13:28:41 +0300

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate ,login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 
 def login(request):
-    return render(request,'accounts/index.html')
+    
+    return render(request,'accounts/index.html')    
 
 def register(request):
     return render(request,'accounts/register.html')
--- lessons/views.py
+++ lessons/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import Lesson
 # Create your views here.
 
 def index(request):
-
-    return render(request,'lesson/index.html')
+    context = Lesson.objects.all()
+    return render(request,'lesson/index.html',context= {
+        'lesson':context
+    })
